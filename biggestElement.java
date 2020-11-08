aaba818cc0574d21a692e7c28b4bcf7d45925c8b
Ivelina Stoyanova <72572005+guilaharis@users.noreply.github.com>
2020-11-08 19:12:29 +0200

    Add files via upload

---
+++ biggestElement.java
@@ -0,0 +1,31 @@
+//Find biggest element of an array and print it in the console
+
+public class biggestElement {
+
+	public static void main(String[] args) {
+		int[] array = {1, 4, 5, 8, 10, 17}; 
+		for (int i = 0; i < 1; i++) {
+			if(array [0]> array [1]) { 
+			continue;
+		}
+		else if (array [1] > array [2]) {
+			continue;}
+		else if (array [2] > array [3]) {
+			continue;
+		}
+		else if (array[3] > array[4]) {
+			continue;
+		}
+		else if (array[4] > array [5]) {
+			continue;
+		}
+		
+		}
+		
+			System.out.println(array[5]);
+	
+		
+
+	}
+
+}
