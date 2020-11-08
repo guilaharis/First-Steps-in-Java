cc3434f944db4bcffd203e63bfb3f9d9071b38ca
Ivelina Stoyanova <72572005+guilaharis@users.noreply.github.com>
2020-11-08 16:28:00 +0200

    Add files via upload

---
+++ sumArray.java
@@ -0,0 +1,16 @@
+//Write program to print sum of a given array elements
+
+
+public class sumArray {
+
+	public static void main(String[] args) {
+	String [] chocolates = {"Milka", "Nestle", "Lindt"}; 
+	for (int i = 0; i < chocolates.length; i++) {
+	System.out.println(chocolates[i]);
+		
+	}
+
+		
+	}
+
+}
