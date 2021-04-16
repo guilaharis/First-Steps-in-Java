b81b0e049d4ceb1f0a636f78b596aabd81cbfe6b
Ivelina Stoyanova <72572005+guilaharis@users.noreply.github.com>
2021-04-16 13:35:55 +0300

    Add files via upload

---
+++ ClassPerson/Bulgarian.java
@@ -0,0 +1,30 @@
+
+public class Bulgarian extends Person {
+
+	public Bulgarian(String personName) {
+		super(personName);
+		
+	}
+
+	public void sayHello(String language) {
+		System.out.println("Zdravei");
+	}
+	
+	public void celebrateEaster(String religion) {
+		System.out.println("Ortodox Easter");
+	}
+	
+	 public void isAdult(String countryOfResidence) {
+		 System.out.println("Pulnoletna e");
+	 }
+	 
+	 public void canTakeLoan(String job) {
+		 System.out.println("Studentski zaem");
+	 }
+	 
+	 public void peeNarodniPesni () {
+		 System.out.println("Polegnala e Todora");
+	 }
+}
+
+
