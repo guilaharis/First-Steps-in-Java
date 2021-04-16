b66cce87d848f962fe4955f987bb4eef63ad45a7
Ivelina Stoyanova <72572005+guilaharis@users.noreply.github.com>
2021-04-16 13:36:20 +0300

    Add files via upload

---
+++ ClassPerson/Italian.java
@@ -0,0 +1,30 @@
+
+public class Italian extends Person {
+
+	public Italian(String personName) {
+		super(personName);
+		
+	}
+	
+	public void sayHello(String language) {
+		System.out.println("Ciao ragazzi");
+	}
+	
+	public void celebrateEaster(String religion) {
+		System.out.println("Catholic Easter");
+	}
+	
+	public void isAdult(String countryOfResidence) {
+		System.out.println("Is adult too");
+	}
+	
+	public void canTakeLoan(String job) {
+		System.out.println("Too many loans");
+	}
+	
+	public void singsOpera () {
+		System.out.println("O sole mio");
+	}
+}
+
+
